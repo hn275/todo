737f19b22d9e997cc90ca08e6866e86240a5d63e
Hal <haln_01@proton.me>
2022-08-27 19:37:58 -0700

    added get.ts

---
+++ client/src/store/todoThunks/get.ts
@@ -0,0 +1,9 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { SERVER_URL } from 'assets/constants';
+import axios from 'axios';
+
+export const handleGetRequest = createAsyncThunk('getAllTodos', async () => {
+  const todos = await axios.get(SERVER_URL);
+  console.log(todos);
+  return todos.data;
+});
