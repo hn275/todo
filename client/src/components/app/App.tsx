d1245727b6e30871d47026ae17b0b86265758ece
Hal <haln_01@proton.me>
2022-08-29 14:41:07 -0700

    project done

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.scss';
 import { handleGetRequest } from 'store/todoThunks/getRequest';
-import { useTodoSelector, useTodoDispatch } from 'hooks/hooks';
+import { useTodoDispatch } from 'hooks/hooks';
 import { DarkMode } from 'features/darkMode/DarkMode';
 import { TodoContainer } from 'container/todoContainer/TodoContainer';
-import { TodoMenu } from 'components/todoMenu/TodoMenu';
 import { TodoFilter } from 'features/todoFilter/TodoFilter';
 import DarkBG from 'assets/images/bg-desktop-dark.jpg';
-import LightBG from 'assets/images/bg-desktop-light.jpg';
 
 function App() {
   const { useEffect, useRef } = React;
@@ -18,11 +16,6 @@ function App() {
     dispatch(handleGetRequest());
   }, []);
 
-  const todolist = useTodoSelector((state) => state.todo.todoList);
-  useEffect(() => {
-    console.log(todolist);
-  }, [todolist]);
-
   return (
     <div
       className='App'
@@ -30,10 +23,10 @@ function App() {
       ref={appNode}
     >
       <div className='background'>
-        <img
-          src={DarkBG}
-          alt='background'
-        />
+        {/* <img */}
+        {/*   src={DarkBG} */}
+        {/*   alt='background' */}
+        {/* /> */}
       </div>
       <div className='wrapper'>
         <header>
--- client/src/features/addNewTodo/AddNewTodo.tsx
+++ client/src/features/addNewTodo/AddNewTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { TextField } from 'components/textField/TextField';
 import { useTodoDispatch } from 'hooks/hooks';
 import { handlePostRequest } from 'store/todoThunks/postRequest';
@@ -6,18 +6,31 @@ import './AddNewTodo.scss';
 
 interface AddNewTodoProps {}
 export const AddNewTodo: React.FC<AddNewTodoProps> = () => {
+  // handler new todo
   const [newTodo, setNewTodo] = useState<string>('');
   const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewTodo(() => event.target.value);
   };
-
   const dispatch = useTodoDispatch();
   const handleSubmit = () => {
     const dispatchNewTodo = { content: newTodo };
     dispatch(handlePostRequest(dispatchNewTodo)); // dispatch action
     setNewTodo(() => ''); // clear input field
   };
+  // event listener
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    function handleReturnKey(this: HTMLElement, e: KeyboardEvent) {
+      if (e.key === 'Enter') {
+        handleSubmit();
+      }
+    }
+    inputRef.current!.addEventListener('keypress', handleReturnKey);
+    return () =>
+      inputRef.current!.removeEventListener('keypress', handleReturnKey);
+  });
 
+  // TSX
   return (
     <>
       <TextField
@@ -30,6 +43,7 @@ export const AddNewTodo: React.FC<AddNewTodoProps> = () => {
           placeholder='Create a new todo...'
           value={newTodo}
           onChange={onInputChange}
+          ref={inputRef}
         />
       </TextField>
     </>
