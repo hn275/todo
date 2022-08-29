14f33a1f439d025370ffae95c6f53096bba977c7
Hal <haln_01@proton.me>
2022-08-29 00:02:08 -0700

    fixed bug in delete request

--- server/src/util/deleteTodo.ts
+++ server/src/util/deleteTodo.ts
@@ -4,7 +4,7 @@ import data from '../data.json';
 import { writeFile } from './writeFile';
 
 export const deleteTodo = (req: TodoParamRequest, res: Response) => {
-  const { ids } = req.body.data;
+  const ids = req.body.ids;
   const newList = data.todoList.filter((todo: Todo) => {
     return !ids.includes(todo.id);
   });
